fcc4d627a9e0c8f773e07a718fa4f72c7cdb7d6c
PlantvZombie <wintonajones@gmail.com>
2024-09-28 13:15:27 -0400

    Update greeb.gd

--- greeb.gd
+++ greeb.gd
@@ -1,10 +1,5 @@
 extends Control
 
-var Option1:bool = false
-var Option2:bool = false
-var Option3:bool = false
-var Option4:bool = false
-
 
 func _on_option_1_pressed():
 	pass # Replace with function body.
