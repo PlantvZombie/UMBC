5795769ca659d4fa53e272e3d7c750b9f505b1d8
PlantvZombie <wintonajones@gmail.com>
2024-09-28 12:48:48 -0400

    Base Greeb Made (Need Global Array)

---
+++ greeb.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var Option1:bool = false
+var Option2:bool = false
+var Option3:bool = false
+var Option4:bool = false
+
+
+func _on_option_1_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_3_pressed():
+	pass # Replace with function body.
+
+
+func _on_option_4_pressed():
+	pass # Replace with function body.
